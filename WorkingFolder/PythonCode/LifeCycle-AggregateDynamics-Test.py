1. previously transition matrix calculation ignores determinstic growth factor. now fixed. 2.retirement now is at 41 (in th standard calibration. G[40] is the big drop in permanent income. but there seems still a little kink. 3. comparisonHARK has a baseline life-cycle model simulated with the same parameters of PIR. it serves a benchmark.

WorkingFolder/PythonCode/LifeCycle-AggregateDynamics-Test.py
@@ -74,6 +74,7 @@ lc_paras_q = copy(lc_paras_Q)
 ## make some modifications 
 lc_paras_y['P'] =np.array([[0.01,0.99],[0.01,0.99]])
 lc_paras_y['unemp_insurance'] = 0.0 
+lc_paras_y['init_b'] = 0.0 
 #lc_paras_y['G'] = np.ones_like(lc_paras_y['G'])
 
 print(lc_paras_y)
@@ -1504,7 +1505,7 @@ A_life = HH.A_life
 C_life = HH.C_life
 
 
-# + code_folding=[]
+# + code_folding=[0]
 ## plot life cycle profile
 
 age_lc = SCF_profile.index
@@ -1512,7 +1513,7 @@ age_lc = SCF_profile.index
 fig, ax = plt.subplots(figsize=(10,5))
 plt.title('Life cycle profile of wealth')
 ax.plot(age_lc[1:],
-       A_life,
+       np.log(A_life),
        'r-',
        label='model')
 
@@ -1549,7 +1550,7 @@ HH.get_lifecycle_dist()
 ap_grid_dist_life,ap_pdfs_dist_life = HH.ap_grid_dist_life,HH.ap_pdfs_dist_life
 cp_grid_dist_life,cp_pdfs_dist_life = HH.cp_grid_dist_life,HH.cp_pdfs_dist_life
 
-# + code_folding=[]
+# + code_folding=[0]
 ## create the dataframe to plot distributions over the life cycle 
 ap_pdfs_life = pd.DataFrame(ap_pdfs_dist_life).T
 cp_pdfs_life = pd.DataFrame(cp_pdfs_dist_life).T
@@ -1587,7 +1588,7 @@ else:
 
 # ### General Equilibrium 
 
-# + code_folding=[0, 12, 37, 40]
+# + code_folding=[0, 9, 12, 37, 40]
 economy_data = [
     ('Z', float64),            
     ('K', float64),             
@@ -1664,10 +1665,18 @@ class CDProduction:
         print('R',R_fake)
 
 
-# + code_folding=[0, 13]
+# + code_folding=[0, 21]
 def unemp_insurance2tax(μ,
                         ue_fraction):
     """
+    input
+    =====
+    μ: replcament ratio
+    ue_fraction: fraction of the working population that is unemployed 
+    output
+    ======
+    tax rate: labor income tax rate that balances government budget paying for uemp insurance 
+    
     under balanced government budget, what is the tax rate on income corresponds to the ue benefit μ
     (1-ue_fraction)x tax + ue_fraction x mu x tax = ue_fraction x mu 
     --> tax = (ue_fraction x mu)/(1-ue_fraction)+ue_fraction x mu
@@ -1937,7 +1946,7 @@ market_OLG_mkv.get_equilibrium_k()
 
 market_OLG_mkv.get_equilibrium_dist()
 
-# + code_folding=[0]
+# + code_folding=[]
 ## plot life cycle profile
 
 age_lc = SCF_profile.index

WorkingFolder/PythonCode/PrepareParameters.py
@@ -9,7 +9,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.13.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -30,19 +30,18 @@ import numba as nb
 age_profile = pd.read_stata('../OtherData/age_profile.dta')   
 
 ## select age range for the model and turn it into an array 
-lc_wages = np.array(age_profile[(age_profile['age']>=25) &(age_profile['age']<=64)]['wage_age'])
+lc_wages = np.array(age_profile[(age_profile['age']>=24) &(age_profile['age']<=64)]['wage_age'])
 #print(str(len(lc_wages)),'years since age 25')
 
 ## growth rates since initial age over life cicle before retirement
 lc_G = lc_wages[1:]/lc_wages[:-1]
-lc_G = np.insert(lc_G,0,1.0) 
 
 T = 40
 L = 60
 ## growth rates after retirement
 
 lc_G_rt = np.ones(L-T)
-lc_G_rt[1] = 0.6
+lc_G_rt[0] = 0.6
 
 
 lc_G_full = np.concatenate([lc_G,lc_G_rt])
@@ -136,8 +135,8 @@ b_SCF, b_wgt = SCF2016['networth2pinc'], SCF2016['wgt']
 ## compute data moments 
 σ_ψ_init_SCF = DescrStatsW(lpinc_SCF_pos, weights=lpinc_SCF_wgt, ddof=1).std
 σ_b_init_SCF = DescrStatsW(lb_SCF_pos, weights=lb_SCF_wgt, ddof=1).std
-b_SCF = DescrStatsW(b_SCF,weights=b_wgt, ddof=1).mean ## quarterly 
-b_q_SCF = b_SCF*4
+b_SCF = DescrStatsW(b_SCF,weights=b_wgt, ddof=1).mean ## annual 
+b_q_SCF = b_SCF*4  ## quarterly  
 # -
 
 # ### subjective profile estiamtion 
@@ -150,7 +149,7 @@ SCE_est_y = SCE_est_y['baseline']
 
 # + code_folding=[]
 ## create a dictionary of parameters 
-life_cycle_paras_q = {'ρ': 1.0, 
+life_cycle_paras_q = {'ρ': 2.0, 
                     'β': 0.98**(1/4), 
                     'P': np.array([[0.18, 0.82],
                                    [0.04, 0.96]]), 
@@ -191,7 +190,7 @@ life_cycle_paras_q = {'ρ': 1.0,
 life_cycle_paras_q
 
 ## create a dictionary of parameters 
-life_cycle_paras_y = {'ρ': 1.0, 
+life_cycle_paras_y = {'ρ': 2.0, 
                     'β': 0.98, 
                     'P': np.array([[0.18, 0.82],
                                    [0.04, 0.96]]), 

WorkingFolder/PythonCode/SCFData.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.2
+#       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -20,7 +20,7 @@
 import numpy as np
 import pandas as pd   #The data package
 import sys            #The code below wont work for any versions before Python 3. This just ensures that (allegedly).
-assert sys.version_info >= (3,5)
+
 
 import requests
 import io
@@ -103,12 +103,12 @@ list(df2016.columns)
 # +
 ### filters and clean variables 
 
-
 df2016 = df2016[(df2016['age']>=25) & (df2016['age']<=85)]
 df2016 = df2016[df2016['income']>0]
 df2016 = df2016[df2016['norminc']>0]
 df2016['lincome'] = np.log(df2016['income'])
 df2016['lnorminc'] = np.log(df2016['norminc'])
+df2016['w2income']=df2016['networth']/ df2016['norminc']
 # -
 
 # ### Life-cycle wealth and income profile 
@@ -134,23 +134,31 @@ fig, axes = joypy.joyplot(df2016,
 ## Life cycle income / wealth profiles 
 
 age = df2016['age'].unique()
-wm = lambda x: np.average(x, weights=df2016.loc[x.index, "wgt"])
 
+wm = lambda x: np.average(x, weights=df2016.loc[x.index, "wgt"])
 
 age_av_wealth = df2016.groupby('age').agg(av_wealth = ('networth',wm))
 age_med_wealth = df2016.groupby('age').agg(med_wealth=('networth','median'))
 
-age_av_lincome = df2016.groupby('age').agg(av_wealth = ('lincome',wm))
-age_med_lincome = df2016.groupby('age').agg(med_wealth=('lincome','median'))
+age_av_w2i = df2016.groupby('age').agg(av_w2i = ('w2income',wm))
+age_med_w2i = df2016.groupby('age').agg(med_w2i=('w2income','median'))
 
-age_av_lnorminc = df2016.groupby('age').agg(av_wealth = ('lnorminc',wm))
-age_med_lnorminc = df2016.groupby('age').agg(med_wealth=('lnorminc','median'))
+age_av_lincome = df2016.groupby('age').agg(av_lincome = ('lincome',wm))
+age_med_lincome = df2016.groupby('age').agg(med_lincome=('lincome','median'))
+
+age_av_lnorminc = df2016.groupby('age').agg(av_lnorminc = ('lnorminc',wm))
+age_med_lnorminc = df2016.groupby('age').agg(med_lnorminc=('lnorminc','median'))
 
 # -
 
 plt.title('Net wealth over life cycle')
 plt.plot(np.log(age_av_wealth),label='average net wealth')
 plt.plot(np.log(age_med_wealth),label='median net wealth')
+plt.legend(loc=0)
+
+plt.title('Net wealth over life cycle')
+plt.plot(np.log(age_av_w2i),label='average net wealth/income ratio')
+plt.plot(np.log(age_med_w2i),label='median net wealth/income ratio')
 plt.legend(loc=0)
 
 plt.title('Income over life cycle')
@@ -163,10 +171,12 @@ plt.plot(np.log(age_av_lnorminc),label='average income')
 plt.plot(np.log(age_med_lnorminc),label='median income')
 plt.legend(loc=0)
 
-# + code_folding=[2]
+# + code_folding=[]
 ## merge all age profiles 
 
 to_merge = [age_med_wealth,
+            age_av_w2i,
+            age_med_w2i,
             age_av_lincome,
             age_med_lincome,
             age_av_lnorminc,
@@ -181,9 +191,10 @@ for  df in to_merge:
                               right_index=True)
     
 SCF_age_profile.to_pickle('data/SCF_age_profile.pkl')
-
-
 # -
+
+SCF_age_profile
+
 
 # ### Wealth inequality 
 
@@ -284,10 +295,7 @@ def figureprefs(data, variable = 'income', labels = False, legendlabels = []):
    
     plt.legend(loc = 2)
 
-years_graph = [df2019, df2016,df2013]
-labels = ['2019', '2016','2013']
+years_graph = [df2016]
+labels = ['2016']
 
 figureprefs(years_graph, variable = 'networth', legendlabels = labels);
-# -
-
-

WorkingFolder/PythonCode/SolveLifeCycle-ComparisonHARK.py
@@ -42,8 +42,10 @@ import scipy.sparse.linalg
 from scipy import linalg as lg 
 from numba.typed import List
 from Utility import cal_ss_2markov
+from copy import copy
 
-# + code_folding=[]
+
+# + code_folding=[0]
 ## figure plotting configurations
 
 mp.rc('xtick', labelsize=11) 
@@ -54,17 +56,18 @@ plt.rc('font',size=11)
 # -
 
 from SolveLifeCycle import LifeCycle, EGM, solve_model_iter
-from PrepareParameters import life_cycle_paras_y as inf_paras
+from PrepareParameters import life_cycle_paras_y 
 
 # ### parameters 
 
 # +
+inf_paras = copy(life_cycle_paras_y)
 inf_paras['G'] =  np.ones_like(inf_paras['G'])
 inf_paras['unemp_insurance'] = 0.0
 inf_paras['P'] = np.array([[0.9,0.1],[0.2,0.8]])
 
 grid_max = 5.0
-grid_size = 50
+grid_size = 30
 
 # + code_folding=[0]
 inf_mkv = LifeCycle(U = inf_paras['U'], ## transitory ue risk
@@ -155,7 +158,6 @@ plt.title('Inifite horizon solution')
 # ## Solving the same model with HARK
 #
 
-from copy import copy
 from HARK.ConsumptionSaving.ConsMarkovModel import MarkovConsumerType
 from HARK.distribution import DiscreteDistribution
 from HARK.ConsumptionSaving.ConsIndShockModel import init_lifecycle
@@ -236,35 +238,32 @@ plt.legend(loc=0)
 # -
 # ## Wealth distribution over life cycle 
 
-lc_paras
-
-# +
+# + code_folding=[]
 ## for life cycle 
 init_life_cycle_new = copy(init_lifecycle)
-lc_paras = inf_paras
+lc_paras = copy(life_cycle_paras_y)
+
+
+years_retire = lc_paras['L']- lc_paras['T']
 
 init_life_cycle_new['T_cycle'] = lc_paras['L']-1   ## minus 1 because T_cycle is nb periods in a life cycle - 1 in HARK 
 init_life_cycle_new['CRRA'] = lc_paras['ρ']
-init_life_cycle_new['T_retire'] = lc_paras['T']
+init_life_cycle_new['T_retire'] = lc_paras['T']-1
 init_life_cycle_new['Rfree'] = lc_paras['R']
 init_life_cycle_new['LivPrb'] = [lc_paras['LivPrb']]*init_life_cycle_new['T_cycle']
-init_life_cycle_new['PermGroFac'] = lc_paras['G']
-init_life_cycle_new['PermShkStd'] = [lc_paras['σ_ψ']]*init_life_cycle_new['T_cycle']
-init_life_cycle_new['TranShkStd'] = [lc_paras['σ_θ']]*init_life_cycle_new['T_cycle']
+init_life_cycle_new['PermGroFac'] = np.ones_like(lc_paras['G'])
+init_life_cycle_new['PermShkStd'] = [lc_paras['σ_ψ']]*init_life_cycle_new['T_retire']+[0.0]*years_retire
+init_life_cycle_new['TranShkStd'] = [lc_paras['σ_θ']]*init_life_cycle_new['T_retire']+[0.0]*years_retire
 init_life_cycle_new['DiscFac'] = lc_paras['β']
 init_life_cycle_new['PermGroFacAgg'] = 1.0
-init_life_cycle_new['aNrmInitMean']= lc_paras['init_b']
+init_life_cycle_new['aNrmInitMean']= np.log(lc_paras['init_b'])
 init_life_cycle_new['aNrmInitStd']= 0.0
-init_life_cycle_new['pLvlInitMean']= 1.0
+init_life_cycle_new['pLvlInitMean']= np.log(1.0)
 init_life_cycle_new['pLvlInitStd']= lc_paras['σ_ψ_init']
 init_life_cycle_new["UnempPrb"] = lc_paras['U']  # to make income distribution when employed
-
-#init_life_cycle_new['aXtraMin'] = a_min+0.00001
-#init_life_cycle_new['aXtraMax'] = a_max
-#init_life_cycle_new['aXtraCount'] = 800
-
-#init_life_cycle_new["MrkvArray"] = [lc_paras['P']]
-#init_life_cycle_new["global_markov"] = False
+init_life_cycle_new['UnempPrbRet'] = 0.0
+init_life_cycle_new['IncUnemp'] = 0.0
+init_life_cycle_new['aXtraMax'] = 5.0
 
 """
 LifeCycleType = MarkovConsumerType(**init_life_cycle_new)
@@ -273,6 +272,8 @@ LifeCycleType.cycles = 1 ## life cycle problem instead of infinite horizon
 LifeCycleType.vFuncBool = False  ## no need to calculate the value for the purpose here 
 """
 # -
+
+print(init_life_cycle_new)
 
 import HARK.ConsumptionSaving.ConsIndShockModel as HARK_model         # The consumption-saving micro model
 from HARK.utilities import plot_funcs_der, plot_funcs              # Some tools
@@ -286,9 +287,9 @@ LifeCyclePop.solve()                            # Obtain consumption rules by ag
 LifeCyclePop.unpack('cFunc')                      # Expose the consumption rules
 
 # Which variables do we want to track
-LifeCyclePop.track_vars = ['aNrm','pLvl','mNrm','cNrm','TranShk']
+LifeCyclePop.track_vars = ['aNrm','pLvl','mNrm','cNrm']
 
-LifeCyclePop.T_sim = lc_paras['L']-1              # Nobody lives to be older than 145 years (=25+120)
+LifeCyclePop.T_sim = lc_paras['L']              
 LifeCyclePop.initialize_sim()                     # Construct the age-25 distribution of income and assets
 LifeCyclePop.simulate()
 # -
@@ -298,11 +299,49 @@ aGro41=LifeCyclePop.history['aLvl'][41]/LifeCyclePop.history['aLvl'][40]
 aGro41NoU=aGro41[aGro41[:]>0.2] # Throw out extreme outliers
 aGro41NoU = aGro41NoU[aGro41NoU[:]<2]
 
+## wealth distribution  
+wealth_dist=plt.hist(np.log(LifeCyclePop.history['aLvl'].flatten()+0.00000001),bins=100)
+
 # Plot the distribution of growth rates of wealth between age 65 and 66 (=25 + 41)
 n, bins, patches = plt.hist(aGro41NoU,50,density=True)
 
-plt.plot(LifeCyclePop.history['aLvl'].mean(axis=1))
+# ## Wealthy over life cycle  
 
-plt.plot(np.log(LifeCyclePop.history['aLvl'].mean(axis=1)))
+A_life = LifeCyclePop.history['aLvl'].mean(axis=1)
 
-plt.plot(LifeCyclePop.history['aLvl'].std(axis=1))
+A_life[0]
+
+# +
+import pandas as pd
+SCF_profile = pd.read_pickle('data/SCF_age_profile.pkl')
+
+SCF_profile['mv_wealth'] = SCF_profile['av_wealth'].rolling(3).mean()
+## plot life cycle profile
+
+age_lc = SCF_profile.index
+
+fig, ax = plt.subplots(figsize=(10,5))
+plt.title('Life cycle profile of wealth and consumption')
+ax.plot(age_lc[1:],
+        np.log(A_life),
+       'r-',
+       label='model from HARK')
+
+#ax.vlines(lc_mkv.T+25,
+#          np.min(A_life),
+#          np.max(A_life),
+#          color='b',
+#          label='retirement')
+
+ax2 = ax.twinx()
+ax2.set_ylim([10.5,15])
+ax2.bar(age_lc[1:],
+        np.log(SCF_profile['av_wealth'][1:]),
+       #'k--',
+       label='SCF (RHS)')
+
+ax.set_xlabel('Age')
+ax.set_ylabel('Log wealth')
+ax2.set_ylabel('Log wealth SCF')
+ax.legend(loc=1)
+ax2.legend(loc=2)

WorkingFolder/PythonCode/SolveLifeCycle.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.2
+#       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -17,7 +17,7 @@
 # ## A life-cycle consumption  problem nder objective/subjective risk perceptions
 #
 # - author: Tao Wang
-# - date: September 2021
+# - date: Feb 2022
 # - this is a companion notebook to the paper "Perceived income risks"
 
 # - This notebook builds on a standard life-cycle consumption model under an incomplete market and extends it to allow subjective belief formation about income risks
@@ -26,34 +26,9 @@
 #       - CRRA utility 
 #       - During work: labor income risk: permanent + MA(1)/persistent/transitory/2-state Markov between UE and EMP or between low and high risk + i.i.d. unemployment shock
 #        -  a deterministic growth rate of permanent income over the life cycle 
-#       - During retirement: receives a constant pension proportional to permanent income (hence still subject to permanent income risks)
+#       - During retirement: receives a constant pension proportional to permanent income (no permanent/transitory income risks)
 #       - A positive probability of death before terminal age 
 #   
-
-# - Objective v.s. subjective
-#      - All state variables are obsersable: permanent income, ma(1) shock or the persistent shock
-#      - What may not be perfectly known is the size and nature of the risks 
-#   1. Perfect understanding case
-#   2. Benchmark case without perfect understanding
-#       - individual observe past income realizations
-#       - overreact to transitory shock realizations
-#           - perceived risk of transitory shock is a weighted average of long-run mean and short-run deviations
-#           - similarly, the same mechanism can be for permanent risks 
-#       - individuals swing between low and high risk perceptions (either about permanent or transitory income risks) following a 2-state Markov 
-
-# - Other bolder extensions
-#   - Extention 1
-#        - subjective determine the degree of persistence or the ma(1) coefficient 
-#        - assymetric extrolation 
-#       
-#   - Extention 2 
-#        - adding aggregate risks, and agents need to learn about the growth rate using cross-sectional experiences
-#        - so subjectively determine the cross-sectional correlation
-#        - form the best guess and the corresponding uncertainty based on the attribution
-#          
-#   - Extenstion 3
-#        - persistence attribution is assymmetric: determined by realized income shocks 
-#        - correlation attribution is assymetric: determined by realized income shocks 
 
 import numpy as np
 import pandas as pd
@@ -134,7 +109,7 @@ lc_data = [
 ]
 
 
-# + code_folding=[]
+# + code_folding=[6]
 @jitclass(lc_data)
 class LifeCycle:
     """
@@ -163,7 +138,7 @@ class LifeCycle:
                  L = 60,             ## life length 85
                  G = np.ones(60),    ## growth factor list of permanent income 
                  shock_draw_size = 8,
-                 grid_max = 2.5,
+                 grid_max = 5.0,
                  grid_size = 50,
                  seed = 456789,
                  theta = 2,               ## assymetric extrapolative parameter
@@ -279,7 +254,10 @@ class LifeCycle:
 
     ## utility function 
     def u(self,c):
-        return c**(1-self.ρ)/(1-ρ)
+        if self.ρ!=1:
+            return c**(1-self.ρ)/(1-ρ)
+        elif self.ρ==1:
+            return np.log(c)
     
     # Marginal utility
     def u_prime(self, c):
@@ -309,7 +287,7 @@ class LifeCycle:
         return np.exp(n_shk)
 
 
-# + code_folding=[5]
+# + code_folding=[]
 ## this function takes the consumption values at different grids of state 
 ###   variables from period t+1, and model class 
 ### and generates the consumption values at t 
@@ -350,7 +328,7 @@ def EGM(aϵ_in,
     ρ = lc.ρ
     Γ = lc.Γ
     ####################################
-    G = lc.G[t+1]  ## get the age specific 
+    G = lc.G[t]  ## get the age specific growth rate, G[T] is the sudden drop in retirement from working age
     ####################################
 
     x = lc.x
@@ -440,7 +418,7 @@ def EGM(aϵ_in,
     return aϵ_out, σ_out
 
 
-# + code_folding=[4]
+# + code_folding=[]
 ## the operator under markov stochastic risks 
 ## now the permanent and transitory risks are different between a good macro and bad macro state 
 
@@ -479,7 +457,7 @@ def EGM_sv(aϵ_in,
     ρ = lc.ρ
     Γ = lc.Γ
     ####################################
-    G = lc.G[t+1]  ## get the age specific 
+    G = lc.G[t]   ## get the age specific 
     ####################################    
     x = lc.x
     λ = lc.λ
@@ -597,7 +575,7 @@ def extrapolate(theta,
     return x_sub
 
 
-# + code_folding=[4]
+# + code_folding=[]
 ## subjective agent
 ### transitory shock affects risk perceptions
 
@@ -635,9 +613,8 @@ def EGM_br(aϵ_in,
     ####################
     ρ = lc.ρ
     Γ = lc.Γ
-    G = lc.G
     ####################################
-    G = lc.G[t+1]  ## get the age specific 
+    G = lc.G[t]   ## get the age specific 
     ####################################  
     x = lc.x
     λ = lc.λ
@@ -1449,7 +1426,7 @@ if __name__ == "__main__":
 
 # ### With a Markov/persistent unemployment state
 
-# + code_folding=[4]
+# + code_folding=[0]
 if __name__ == "__main__":
 
 
@@ -1464,7 +1441,7 @@ if __name__ == "__main__":
     #                    (0.05, 0.95)])   # markov transition matricies 
 
 
-# + code_folding=[1]
+# + code_folding=[0]
 if __name__ == "__main__":
 
 
@@ -1484,7 +1461,7 @@ if __name__ == "__main__":
                          unemp_insurance = 0.3,
                          ue_markov = True)
 
-# + code_folding=[]
+# + code_folding=[0]
 if __name__ == "__main__":
 
     ## solve the model for different transition matricies of UE markov
@@ -1525,7 +1502,7 @@ if __name__ == "__main__":
 
     print("Time taken, in seconds: "+ str(t_finish - t_start))
 
-# + code_folding=[11]
+# + code_folding=[0]
 if __name__ == "__main__":
 
 
@@ -1567,6 +1544,7 @@ if __name__ == "__main__":
 # - unemployed perceive higher risks
 #
 
+# + code_folding=[0]
 if __name__ == "__main__":
 
 
@@ -1665,7 +1643,7 @@ if __name__ == "__main__":
 
     print("Time taken, in seconds: "+ str(t_finish - t_start))
 
-# + code_folding=[]
+# + code_folding=[0]
 if __name__ == "__main__":
 
 
@@ -1703,7 +1681,7 @@ if __name__ == "__main__":
 
 # ### Objective and subject state-dependent profile
 
-# + code_folding=[]
+# + code_folding=[0]
 if __name__ == "__main__":
 
     ## compare subjective and objective models 

WorkingFolder/PythonCode/SubjectiveProfileEst.py
@@ -144,7 +144,7 @@ def mkv2_Y2Q(q,
 
 
 
-# + code_folding=[0, 15, 47, 62, 69, 78, 82, 104, 124, 132]
+# + code_folding=[15, 47, 62, 69, 78, 82, 104, 124, 132]
 class Markov2Switching:
     """
     A class that stores primitives for the Markov Regime Switching Model
@@ -599,8 +599,8 @@ vars_demog_sub = ['Q32',  ## age
                   'Q33',  ## gender 
                   'Q36',  ## education (1-8 low to high, 9 other)
                   'educ_gr',##education group (1-3)
-                  'byear',
-                 'nlit'] ## year of birth
+                  'byear', ## year of birth
+                 'nlit'] 
 
 vars_all_reg_long = (vars_id+moms_nom + moms_real+ue_risks+vars_demog+vars_demog_sub+vars_job)
 
@@ -642,7 +642,6 @@ SCEM['age4']=SCEM['age']**4
 
 SCEM['lrincvar'] = np.log(SCEM['rincvar'])
 
-
 ### some data transformation 
 
 SCEM['UE_s'] = SCEM['UE_s']/100
@@ -653,11 +652,14 @@ SCEM['E2E_prob'] = 1- SCEM['UE_s']*(1-SCEM['UE_f'])   ## 1- prob(loses the job a
 
 
 ## trucate 0 and 1s for probs 
-SCEM['U2U_prob_truc'] = np.where((SCEM['U2U_prob']==1),1-1e-10,SCEM['U2U_prob'])
-SCEM['U2U_prob_truc'] = np.where((SCEM['U2U_prob']==0),0+1e-10,SCEM['U2U_prob'])
+SCEM['U2U_prob_truc'] = SCEM['U2U_prob']
+SCEM['U2U_prob_truc'] = SCEM['U2U_prob_truc'].mask(SCEM['U2U_prob_truc']>=1.0,0.99)
+SCEM['U2U_prob_truc'] = SCEM['U2U_prob_truc'].mask(SCEM['U2U_prob_truc']<=0.0,0.001)
 
-SCEM['E2E_prob_truc'] = np.where((SCEM['E2E_prob']==1),1-1e-10,SCEM['E2E_prob'])
-SCEM['E2E_prob_truc'] = np.where((SCEM['E2E_prob']==0),0+1e-10,SCEM['E2E_prob'])
+
+SCEM['E2E_prob_truc'] = SCEM['E2E_prob']
+SCEM['E2E_prob_truc'] = SCEM['E2E_prob_truc'].mask(SCEM['E2E_prob_truc']>=1.0,0.99)
+SCEM['E2E_prob_truc'] = SCEM['E2E_prob_truc'].mask(SCEM['E2E_prob_truc']<=0.0,0.001)
 
 
 ### transform 0-1 prob to a R 
@@ -665,35 +667,48 @@ SCEM['E2E_prob_truc'] = np.where((SCEM['E2E_prob']==0),0+1e-10,SCEM['E2E_prob'])
 prob_inv_func = lambda x: np.log(x)-np.log(1-x)
 SCEM['U2U_prob_e'] = SCEM['U2U_prob_truc'].apply(prob_inv_func)
 SCEM['E2E_prob_e'] =  SCEM['E2E_prob_truc'].apply(prob_inv_func)
+# -
 
+
+## filter by numeracy score
+print('nb of observations before:',str(len(SCEM)))
+SCEM =SCEM[SCEM['nlit']>=2.0]
+print('nb of observations before:',str(len(SCEM)))
 
 # + code_folding=[0]
 ## first step regression
 
 vars_list = ['lrincvar',
-            'U2U_prob',
-            'E2E_prob']  
+            'U2U_prob_e',
+            'E2E_prob_e']  
 
 for var in vars_list:
     ## demeaned 
     SCEM[var+'_dm'] = SCEM[var]-SCEM.groupby('userid')[var].transform('mean')
     
     ## run a panel regression to get the residuls 
-    model = smf.ols(formula = var+'~ C(date)+C(HHinc)+C(gender)+age2+age3+age4+C(educ_gr)',
+    #model = smf.ols(formula = var+'~ C(date)+C(HHinc)+C(gender)+age2+age3+age4+C(educ_gr)',
+    #            data = SCEM)
+    model = smf.ols(formula = var+'_dm~ C(date)',
                 data = SCEM)
     result = model.fit()
     residuls = result.resid
     SCEM[var+'_rd'] = residuls
 
 
+# +
+## correlation 
+
+SCEM[vars_list].corr()
+
 # + code_folding=[]
 ## convert the panel data of rincvar into a list of time series sequence
 vars_rd_list = ['lrincvar_rd',
-                'U2U_prob',
-                'E2E_prob']  
+                'U2U_prob_e_rd',
+                'E2E_prob_e_rd']  
 
 
-# + code_folding=[]
+# + code_folding=[0]
 ## convert it to a list of arrays storing all time series data for each individual
 
 SCEM_sub = SCEM[['userid']+vars_rd_list].dropna(how='any')
@@ -706,18 +721,32 @@ SCE_list = [x for x in SCE_list if ~np.isnan(np.array(x)).any() and x.shape[1]>=
 
 print('how many invidividuals have answers in successive months?',len(SCE_list))
 
-# + code_folding=[5]
+# + code_folding=[0]
+## correlation 
+
+SCEM_sub[vars_rd_list].corr()
+
+# + code_folding=[0]
 ## plot the simulated data 
 random_id = np.random.randint(0,len(SCE_list)-1)
 
-plt.figure(figsize=(10,5))
-plt.title("A random respondent's earning risk perceptions from the survey")
-plt.plot(SCE_list[random_id][0,:],
+fig, axes = plt.subplots(3,1,figsize=(10,8))
+axes[0].set_title("A random respondent's risk perceptions from the survey")
+axes[0].plot(SCE_list[random_id][0,:],
          'o-',
-         label=r'$y_t$')
-plt.legend(loc=1)
+         label='perceived earnign risk')
+axes[0].legend(loc=1)
+axes[1].plot(SCE_list[random_id][1,:],
+         'o-',
+         label='perceived U2E prob')
+axes[1].legend(loc=1)
+axes[2].plot(SCE_list[random_id][2,:],
+         'o-',
+         label='perceived E2E prob')
+axes[2].legend(loc=1)
+fig.savefig('../Graphs/sce/markov_example.png')
 
-# + code_folding=[1, 8]
+# + code_folding=[0, 1]
 ## create the model 
 SCE_mkv2 = Markov2Switching(AR=0,
                             paras = np.array([0.1,0.1,0.1,0.7,0.7,
@@ -730,9 +759,8 @@ SCE_obj = lambda para: -SCE_mkv2.log_likelihood(SCE_list,
                                                 para)[0]   ## only the first output
 
 
-# + code_folding=[0, 20, 29]
-## impose some bounds for some parameter based on informed priors
-
+# + code_folding=[]
+## impose some bounds for some parameters with sensible priors
 
 ## the size of the shock cannot exceed the sample variation
 sigma_ub0 = np.mean([np.std(np.array(x[0,:])) for x in SCE_list])
@@ -744,20 +772,25 @@ sigma_inv_ub1 = SCE_mkv2.exp_func_inv(sigma_ub1)
 sigma_ub2 = np.mean([np.std(np.array(x[2,:])) for x in SCE_list])
 sigma_inv_ub2 = SCE_mkv2.exp_func_inv(sigma_ub1)
 
-## staying probabilities of both 2 states are above half, indicating persistency 
+## staying probabilities of both 2 states are above half 
 q_lb = 0.5  ## persistent 
-q_inv_lb =SCE_mkv2.prob_func_inv(q_lb) 
+q_inv_lb = SCE_mkv2.prob_func_inv(q_lb) 
 p_lb = 0.5 ## persistent 
-p_inv_lb =SCE_mkv2.prob_func_inv(p_lb)
+p_inv_lb = SCE_mkv2.prob_func_inv(p_lb)
 
 ## estimation 
 guess = np.array([0.2,0.4,-0.5,0.1,0.4,
                 0.2,0.4,-2,0.1,0.4,
                 0.2,-0.1,-2,0.1,0.4])
 
-bounds = ((None,None),(0.0,None),(-1,sigma_inv_ub0),(q_inv_lb,None),(p_inv_lb,None),
-         (None,None),(0.0,None),(-4,sigma_inv_ub1),(None,None),(None,None),
-         (None,None),(None,0.0),(-4,sigma_inv_ub2),(None,None),(None,None),)
+
+bounds = ((None,None),(0.0,None),(-1,sigma_inv_ub0),(q_lb,None),(p_inv_lb,None),
+         (None,None),(0.0,None),(-1,sigma_inv_ub1),(None,None),(None,None),
+         (None,None),(None,0.0),(-1,sigma_inv_ub2),(None,None),(None,None),)
+
+#bounds = ((None,None),(0.0,None),(-1,sigma_inv_ub0),(q_inv_lb,None),(p_inv_lb,None),
+#         (None,None),(0.0,None),(-2,sigma_inv_ub1),(None,None),(None,None),
+#         (None,None),(None,0.0),(-2,sigma_inv_ub2),(None,None),(None,None),)
 
 
 result = minimize(SCE_obj,
@@ -765,17 +798,15 @@ result = minimize(SCE_obj,
                   method='SLSQP',   #SLSQP
                   bounds = bounds,
                   options={'disp': True,
-                            'maxiter':5000}
+                            'maxiter':20000}
                    )
 print('success? ',result['success'])
 SCE_para_est = result['x']
 ## get alpha, beta, sigma, q, p 
 
-
 ## get model parameters 
 guess_para_model = SCE_mkv2.make_para_dict(guess)
 SCE_para_model_est = SCE_mkv2.make_para_dict(SCE_para_est)
-
 
 #results
 print("initial guess of the parameters\n",guess_para_model)
@@ -792,7 +823,7 @@ kappa_sipp = np.median(kappas_sipp.dropna())
 kappa = kappa_sipp ## ratio of permanent and transitory risks 
 
 
-# + code_folding=[0]
+# + code_folding=[8]
 ## create a dictionary for storing QUARTERLY parameters 
 
 model_para_q_est = {}
@@ -812,12 +843,18 @@ model_para_q_est['\tilde\sigma_\theta^l'] =  1/3*model_para_q_est['\tilde\sigma_
 model_para_q_est['\tilde\sigma_\psi^h'] =  np.sqrt(3*np.exp(alpha+beta)/(12+1/(12*kappa**2)))
 model_para_q_est['\tilde\sigma_\theta^h'] =  1/3*model_para_q_est['\tilde\sigma_\psi^h']/kappa
 
+alpha_q = SCE_para_model_est['α'][1]
+beta_q = SCE_para_model_est['α'][1]+SCE_para_model_est['β'][1]
 
-model_para_q_est['\tilde \mho^l'],model_para_q_est['\tilde E^l'] = mkv2_Y2Q(SCE_para_model_est['α'][1],
-                                                                        SCE_para_model_est['α'][2])
+alpha_p = SCE_para_model_est['α'][2]
+beta_p = SCE_para_model_est['α'][2]+SCE_para_model_est['β'][2]
 
-model_para_q_est['\tilde \mho^h'], model_para_q_est['\tilde E^h']=  mkv2_Y2Q(SCE_para_model_est['α'][1]+SCE_para_model_est['β'][1],
-                                                                         SCE_para_model_est['α'][2]+SCE_para_model_est['β'][2])
+
+model_para_q_est['\tilde \mho^l'],model_para_q_est['\tilde E^l'] = mkv2_Y2Q(SCE_mkv2.prob_func(alpha_q),
+                                                                        SCE_mkv2.prob_func(alpha_p))
+
+model_para_q_est['\tilde \mho^h'], model_para_q_est['\tilde E^h']=  mkv2_Y2Q(SCE_mkv2.prob_func(alpha_q+beta_q),
+                                                                         SCE_mkv2.prob_func(alpha_p+beta_p))
 
 print('quarterly SCE parameters\n',model_para_q_est)
 
@@ -875,19 +912,22 @@ index_names = ['$q$',
 
 
 SCE_para_est_q_df.index = index_names
-SCE_para_est_q_df
 
 
 ## save it to a pickle file 
 SCE_para_est_q_df.to_pickle('data/subjective_profile_est_q.pkl')
 
-# + code_folding=[0, 32, 67]
+SCE_para_est_q_df
+
+
+
+# + code_folding=[39, 74]
 ## create a dictionary for storing YEARLY parameters 
 
 model_para_y_est = {}
 
 ############################################
-## from yeraly to monthly risk then to quarterly 
+## from yeraly to monthly risk
 ############################################
 
 ###!!!! Here you need to transform montly mkv to 1 year
@@ -904,9 +944,16 @@ model_para_y_est['\tilde\sigma_\psi^h'] =  np.sqrt(12*np.exp(alpha+beta)/(12+1/(
 model_para_y_est['\tilde\sigma_\theta^h'] =  1/12*model_para_q_est['\tilde\sigma_\psi^h']/kappa
 
 
-model_para_y_est['\tilde \mho^l'],model_para_y_est['\tilde E^l'] = SCE_para_model_est['α'][1],SCE_para_model_est['α'][2]
+alpha_q = SCE_para_model_est['α'][1]
+beta_q = SCE_para_model_est['α'][1]+SCE_para_model_est['β'][1]
 
-model_para_y_est['\tilde \mho^h'], model_para_y_est['\tilde E^h']=  SCE_para_model_est['α'][1]+SCE_para_model_est['β'][1],SCE_para_model_est['α'][2]+SCE_para_model_est['β'][2]
+alpha_p = SCE_para_model_est['α'][2]
+beta_p = SCE_para_model_est['α'][2]+SCE_para_model_est['β'][2]
+
+
+model_para_y_est['\tilde \mho^l'],model_para_y_est['\tilde E^l'] = SCE_mkv2.prob_func(alpha_q),SCE_mkv2.prob_func(alpha_p)
+
+model_para_y_est['\tilde \mho^h'], model_para_y_est['\tilde E^h']=  SCE_mkv2.prob_func(alpha_q+beta_q),SCE_mkv2.prob_func(alpha_p+beta_p)
 
 print('yearly SCE parameters\n',model_para_y_est)
 
@@ -961,11 +1008,13 @@ index_names = ['$q$',
               '$\tilde E^h$']
 
 SCE_para_est_y_df.index = index_names
-SCE_para_est_y_df
 
 
 ## save it to a pickle file 
 SCE_para_est_y_df.to_pickle('data/subjective_profile_est_y.pkl')
+
+SCE_para_est_y_df
+
 
 
 # + code_folding=[5]
@@ -991,126 +1040,139 @@ with open("parameters.txt", "wb") as fp:
 #
 
 # + code_folding=[7, 11]
-import pandas_datareader.data as web
-import datetime
+if __name__ == "__main__":
 
-start = datetime.datetime(1945, 1, 1)
-end = datetime.datetime(2019, 12, 31)
+    import pandas_datareader.data as web
+    import datetime
+
+    start = datetime.datetime(1945, 1, 1)
+    end = datetime.datetime(2019, 12, 31)
 
 
-rec = web.DataReader("USREC", 
-                     "fred", 
-                     start, 
-                     end)
-gdp = web.DataReader('GDPC1',
-                     'fred',
-                     start,
-                     end)
-gdp_gr = gdp.pct_change().dropna()
-gdp_gr.index = pd.DatetimeIndex(gdp_gr.index, freq="QS")
+    rec = web.DataReader("USREC", 
+                         "fred", 
+                         start, 
+                         end)
+    gdp = web.DataReader('GDPC1',
+                         'fred',
+                         start,
+                         end)
+    gdp_gr = gdp.pct_change().dropna()
+    gdp_gr.index = pd.DatetimeIndex(gdp_gr.index, freq="QS")
 
 
 # + code_folding=[2]
-## use statsmodels 
+if __name__ == "__main__":
 
-mod_hamilton = sm.tsa.MarkovAutoregression(
-    gdp_gr, 
-    k_regimes=2, 
-    order= 4, 
-    switching_ar=False
-)
-res_hamilton = mod_hamilton.fit()
+
+    ## use statsmodels 
+
+    mod_hamilton = sm.tsa.MarkovAutoregression(
+        gdp_gr, 
+        k_regimes=2, 
+        order= 4, 
+        switching_ar=False
+    )
+    res_hamilton = mod_hamilton.fit()
 # -
 
-res_hamilton.summary()
+if __name__ == "__main__":
+    res_hamilton.summary()
 
-# +
-fig, axes = plt.subplots(2, figsize=(7, 7))
-ax = axes[0]
-ax.plot(res_hamilton.filtered_marginal_probabilities[0])
-ax.fill_between(rec.index, 
-                0, 
-                1, 
-                where=rec["USREC"].values, 
-                color="k", 
-                alpha=0.1)
-ax.set_xlim(gdp_gr.index[4], gdp_gr.index[-1])
-ax.set(title="Filtered probability of recession")
-
-ax = axes[1]
-ax.plot(res_hamilton.smoothed_marginal_probabilities[0])
-ax.fill_between(rec.index, 0, 1, where=rec["USREC"].values, color="k", alpha=0.1)
-ax.set_xlim(gdp_gr.index[4], gdp_gr.index[-1])
-ax.set(title="Smoothed probability of recession")
-
-fig.tight_layout()
-
-# +
-## estimation using codes here 
-
-mkv2_gdp = Markov2Switching(AR=1,
-                           nb_var=1)
-fake_data_list = [np.array(gdp_gr['GDPC1'])]
-
-obj = lambda para: -mkv2_gdp.log_likelihood(fake_data_list,
-                                        para)[0]   ## only the first output
-
-## estimation 
-guess = (0.0,0.2,0.4,0.2,0.2,0.9)
-#bounds = [(None,None),(None,None),(0.03,1.0),(0.0,1.0),(0.0,1.0)] 
-
-para_est = minimize(obj,
-                    x0=guess,
-                    method='CG',
-                    options={'disp': True,
-                            #'maxls':40,
-                            #'ftol': 2.220446049250313e-11
-                            }
-                   )['x']
-
-#results
-print('Estimated model parameters:',[round(x,2) for x in mkv2_gdp.get_model_para(para_est)])
-
-llh,filter1,pr1,pdf = mkv2_gdp.log_likelihood(fake_data_list,
-                                          para_est)
+if __name__ == "__main__":
 
 
-# + code_folding=[]
-## plot the time series 
+    fig, axes = plt.subplots(2, figsize=(7, 7))
+    ax = axes[0]
+    ax.plot(res_hamilton.filtered_marginal_probabilities[0])
+    ax.fill_between(rec.index, 
+                    0, 
+                    1, 
+                    where=rec["USREC"].values, 
+                    color="k", 
+                    alpha=0.1)
+    ax.set_xlim(gdp_gr.index[4], gdp_gr.index[-1])
+    ax.set(title="Filtered probability of recession")
 
-lw = 2
-figsize = (15,5)
-fontsize = 10
+    ax = axes[1]
+    ax.plot(res_hamilton.smoothed_marginal_probabilities[0])
+    ax.fill_between(rec.index, 0, 1, where=rec["USREC"].values, color="k", alpha=0.1)
+    ax.set_xlim(gdp_gr.index[4], gdp_gr.index[-1])
+    ax.set(title="Smoothed probability of recession")
 
-## plot 
-fig, ax = plt.subplots(figsize = figsize)
-ax2 = ax.twinx()
-ax.plot(gdp_gr.index[2:-1],
-        gdp_gr[2:-1],
-        color='black',
-           lw= lw,
-           label= 'gdp growth ')
-ax2.plot(gdp_gr.index[2:-1],
-         filter1[0][2:-1],
-         'r-',
-         lw = lw,
-         label = 'filter prob (RHS)')
-ax.legend(loc = 2,
-         fontsize = fontsize)
-ax.set_xlabel("month",fontsize = fontsize)
-ax.grid()
-ax.set_ylabel('% growth',fontsize = fontsize)
-ax.tick_params(axis='both', 
-               which='major', 
-               labelsize = fontsize)
-ax2.tick_params(axis='both', 
-               which='major', 
-               labelsize = fontsize)
-ax2.fill_between(rec.index, 
-                min(filter1[0][1:-1]), 
-                max(filter1[0][1:-1]), 
-                where=rec["USREC"].values, 
-                color="k", 
-                alpha=0.1)
-ax2.legend(loc = 1,
-          fontsize = fontsize)
+    fig.tight_layout()
+
+if __name__ == "__main__":
+
+
+    ## estimation using codes here 
+
+    mkv2_gdp = Markov2Switching(AR=1,
+                               nb_var=1)
+    fake_data_list = [np.array(gdp_gr['GDPC1'])]
+
+    obj = lambda para: -mkv2_gdp.log_likelihood(fake_data_list,
+                                            para)[0]   ## only the first output
+
+    ## estimation 
+    guess = (0.0,0.2,0.4,0.2,0.2,0.9)
+    #bounds = [(None,None),(None,None),(0.03,1.0),(0.0,1.0),(0.0,1.0)] 
+
+    para_est = minimize(obj,
+                        x0=guess,
+                        method='CG',
+                        options={'disp': True,
+                                #'maxls':40,
+                                #'ftol': 2.220446049250313e-11
+                                }
+                       )['x']
+
+    #results
+    print('Estimated model parameters:',[round(x,2) for x in mkv2_gdp.get_model_para(para_est)])
+
+    llh,filter1,pr1,pdf = mkv2_gdp.log_likelihood(fake_data_list,
+                                              para_est)
+
+
+# + code_folding=[0]
+if __name__ == "__main__":
+
+
+    ## plot the time series 
+
+    lw = 2
+    figsize = (15,5)
+    fontsize = 10
+
+    ## plot 
+    fig, ax = plt.subplots(figsize = figsize)
+    ax2 = ax.twinx()
+    ax.plot(gdp_gr.index[2:-1],
+            gdp_gr[2:-1],
+            color='black',
+               lw= lw,
+               label= 'gdp growth ')
+    ax2.plot(gdp_gr.index[2:-1],
+             filter1[0][2:-1],
+             'r-',
+             lw = lw,
+             label = 'filter prob (RHS)')
+    ax.legend(loc = 2,
+             fontsize = fontsize)
+    ax.set_xlabel("month",fontsize = fontsize)
+    ax.grid()
+    ax.set_ylabel('% growth',fontsize = fontsize)
+    ax.tick_params(axis='both', 
+                   which='major', 
+                   labelsize = fontsize)
+    ax2.tick_params(axis='both', 
+                   which='major', 
+                   labelsize = fontsize)
+    ax2.fill_between(rec.index, 
+                    min(filter1[0][1:-1]), 
+                    max(filter1[0][1:-1]), 
+                    where=rec["USREC"].values, 
+                    color="k", 
+                    alpha=0.1)
+    ax2.legend(loc = 1,
+              fontsize = fontsize)

WorkingFolder/PythonCode/Utility.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.2
+#       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -146,5 +146,33 @@ def make_grid_exp_mult(ming, maxg, ng, timestonest=20):
         Lgrid = np.arange(Lming, Lmaxg + 0.000001, Lstep)
         grid = np.exp(Lgrid)
     return grid
+
+
+# + code_folding=[1]
+## lorenz curve
+def lorenz_curve(grid_distribution,
+                 pdfs,
+                 nb_share_grid = 50):
+    """
+    parameters
+    ======
+    grid_distribution: grid on which distribution is defined
+    pdfs: the fractions/pdfs of each grid ranges 
+    
+    return
+    ======
+    lc_vals: the fraction of people corresponding whose total wealth reaches the corresponding share, x axis in lorenz curve
+    share_grids: different grid points of the share of total wealth, y axis in lorenz curve
+    """
+    total = np.dot(grid_distribution,pdfs)
+    share_grids = np.linspace(0.0,0.99,nb_share_grid)
+    share_cum = np.multiply(grid_distribution,pdfs).cumsum()/total
+    lc_vals = []
+    for i,share in enumerate(share_grids):
+        where = min([x for x in range(len(share_cum)) if share_cum[x]>=share])
+        this_lc_val = pdfs[0:where].sum()
+        lc_vals.append(this_lc_val)
+    return np.array(lc_vals),share_grids
+
 
 